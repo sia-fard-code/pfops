2b596e049727e4aa52cfb714edd2330aae94d1c1
Sia Fard <sia.gfard.code@gmail.com>
2022-12-19 22:17:24 -0800

    fixed up

--- docker-build.sh
+++ docker-build.sh
@@ -1,5 +1,5 @@
-docker build -t siavashghf2000/pfops Dockerfile.pfops
-docker build -t siavashghf2000/argocd Dockerfile.argocd
+docker build -t siavashghf2000/pfops . -f Dockerfile.pfops
+docker build -t siavashghf2000/argocd . -f Dockerfile.argocd
 
 docker push siavashghf2000/argocd
 docker push siavashghf2000/pfops
